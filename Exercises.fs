f68f168ab6e391a4ed567518c994f4bd189a36eb
Mikkel Bistrup Andersen <mbia@itu.dk>
2023-02-20 08:47:53 +0100

    Solved 3.1 and added tests

--- Exercises.fs
+++ Exercises.fs
@@ -8,12 +8,20 @@ let (.+.) a b = Add (a, b)
 let (.-.) a b = Sub (a, b)
 let (.*.) a b = Mul (a, b)
 
+//Tests
+let a1 = N 42;;
+let a2 = N 4 .+. (N 5 .-. N 6);;
+let a3 = N 4 .*. N 2 .+. N 34;;
+let a4 = (N 4 .+. N 2) .*. N 34;;
+let a5 = N 4 .+. (N 2 .*. N 34);;
+
 //Exercise 3.1
-let rec arithEvalSimple (a: aExp) =
+let rec arithEvalSimple a =
     match a with 
-    | N -> N
-    | Add (a,b) -> arithEvalSimple a + arithEvalSimple b
+    | N n -> n
+    | Add (a,b) -> (arithEvalSimple a) + ( arithEvalSimple b)
     | Sub (a,b) -> arithEvalSimple a - arithEvalSimple b
     | Mul (a,b) -> arithEvalSimple a * arithEvalSimple b
 
+//Exercise 3.2
 
